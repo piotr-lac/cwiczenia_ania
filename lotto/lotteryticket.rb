f8317ad2843fadb8bf17c381171f51e73dc9ea17
Piotr Lac <piotr.lac@protonmail.ch>
2017-11-16 07:09:21 +0100

    lotto

--- lotto/lotteryticket.rb
+++ lotto/lotteryticket.rb
@@ -4,7 +4,7 @@ class LotteryTicket
 	attr_accessor :ticket_numbers
 	def initialize(*given_numbers) 
 		@ticket_numbers = given_numbers 
-		@ticket_numbers = RandomSix.new.random_score if @ticket_numbers.length < 6
+		@ticket_numbers = RandomSix.new.random_score if @ticket_numbers.length == 0
 	end #end of initialize	
 end #end of class
 
